0c8e64de5558bd9dee649b5a88acc5c38dbf0b8a
William Gravina <joaquimbenedito.livro@gmail.com>
2026-07-29 04:10:44 -0300

    chore(api-server): commit nest new scaffold baseline

---
+++ apps/api-server/src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common'
+
+@Module({
+  imports: [],
+  controllers: [],
+  providers: []
+})
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class -- this is a module
+export class AppModule {}
